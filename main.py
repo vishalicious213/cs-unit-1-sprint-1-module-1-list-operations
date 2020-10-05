82f5fb199f8b9c0d4fed1d235d29e757c76464be
Vish Singh <vishalicious213@gmail.com>
2020-10-05 21:22:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+1. Create a "numbers" list that contains two different integer values and two different floating point values.
+
+2. Create a "strings" list that contains three different strings.
+
+3. Print the 4th number of your numbers list and the 1st string of your strings list.
+
+4. Iterate through your strings list and print each string.
+"""
+
+# YOUR CODE HERE
+numbers = [1, 2, 3.4, 5.6]
+strings = ["Lest", "We", "Forget"]
+print(numbers[3], strings[0])
+
+for eachWord in strings:
+  print(eachWord)
